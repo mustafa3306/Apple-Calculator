3268f86e7608aa2db0fc0878601ac0ff5c219190
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-07 14:24:22 +0300

    edits

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 let hesapyeri = document.querySelector(".buttons");
-let altkisim = document.querySelector(".downside")
-let ustkisim = document.querySelector(".upside")
+let altkisim = document.querySelector(".downside");
+let ustkisim = document.querySelector(".upside");
+let silme = document.querySelector(".silme")
 
-// sayıların seçimi
+// tek tek butonlara işlem atama
 hesapyeri.addEventListener("click", (e) => {
     if(e.target.classList.contains("num")){
-        console.log(e.target.innerText);
-        altkisim.innerText = e.target.innerText;
-        ustkisim.innerText = "burhan"    
+        altkisim.innerText += e.target.innerText; 
+    }
+    else if ( e.target.classList.contains("op")){
+        ustkisim.innerText = altkisim.innerText + " " + e.target.innerText;
+        altkisim.innerText = "";
+    }
+    else if (e.target == document.querySelector(".silme")){
+        altkisim.innerText = ""
+        ustkisim.innerText = ""
     } 
 });
